647f8f790c116e028ad02b9437914528490e5e17
Aakash <116252032+Aakashbalamurugan@users.noreply.github.com>
2023-07-11 20:10:52 +0530

    day04 of core java

---
+++ src/main/java/day04/mics/mammel/Hare.java
@@ -0,0 +1,17 @@
+package day04.mics.mammel;
+
+public class Hare {
+
+	public void init() {
+
+		System.out.println("init-");
+
+	}
+
+	protected void race() {
+
+		System.out.println("hare-");
+
+	}
+
+}
---
+++ src/main/java/day04/solved/Animal1.java
@@ -0,0 +1,33 @@
+package day04.solved;
+
+
+
+/**
+ * @author Vinit Gore
+ *
+ **/
+public class Animal1 {
+	private int age;
+	private String name;
+	
+	public Animal1(String name) {
+		System.out.println("Calling Parent Constructor");
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+}
